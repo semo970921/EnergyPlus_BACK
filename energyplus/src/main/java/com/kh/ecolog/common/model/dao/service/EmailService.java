9e2cc4a43a090d958dad36275bf846880219c25f
SeungWon <semo970921@naver.com>
2025-04-24 11:11:55 +0900

    feat/이메일 및 이메일 인증 서비스 인터페이스 구현

---
+++ energyplus/src/main/java/com/kh/ecolog/common/model/dao/service/EmailService.java
@@ -0,0 +1,20 @@
+package com.kh.ecolog.common.model.dao.service;
+
+public interface EmailService {
+	
+	/**
+	 * 이메일 발송 메서드
+	 * @param to 수신자 이메일
+	 * @param subject 이메일 제목
+	 * @param content 이메일 내용
+	 */
+	void sendEmail(String to, String subject, String content);
+	
+	/**
+	 * 회원가입 이메일 인증코드 발송
+	 * @param to
+	 * @param verificationCode 인증코드
+	 */
+	void sendSignUpVerificationEmail(String to, String verificationCode);
+
+}
---
+++ energyplus/src/main/java/com/kh/ecolog/common/model/dao/service/VerificationService.java
@@ -0,0 +1,33 @@
+package com.kh.ecolog.common.model.dao.service;
+
+public interface VerificationService {
+
+	/**
+	 * 인증 코드 생성 및 저장
+	 * @param email
+	 * @return
+	 */
+	String generateVerificationCode(String email);
+	
+	/**
+	 * 인증 코드 검증
+	 * @param email 
+	 * @param code
+	 * @return 검증결과(t->유효한 코드, f->유효하지 않은 코드)
+	 */
+	boolean verifyCode(String email, String code);
+	
+	/**
+	 * 이메일이 인증되었는지 확인
+	 * @param email 확인할 이메일
+	 * @return 인증여부(t->인증됨, f->인증되지않음)
+	 */
+	boolean isEmailVerified(String email);
+	
+	/**
+	 * 인증된 이메일 제거
+	 * @param email
+	 */
+	void removeVerifiedEmail(String email);
+	
+}
