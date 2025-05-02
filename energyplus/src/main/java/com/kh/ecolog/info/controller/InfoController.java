9e498ca52b29e6c73b9345a4af4868d483287d9a
haruki975-gif <haruki975@gmail.com>
2025-05-02 09:13:37 +0900

    feat/pwchange

--- energyplus/src/main/java/com/kh/ecolog/info/controller/InfoController.java
+++ energyplus/src/main/java/com/kh/ecolog/info/controller/InfoController.java
@@ -1,5 +1,6 @@
 package com.kh.ecolog.info.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kh.ecolog.info.model.dto.InfoDTO;
+import com.kh.ecolog.info.model.dto.PasswordDTO;
 import com.kh.ecolog.info.model.service.InfoService;
 
 import jakarta.validation.Valid;
@@ -35,5 +37,28 @@ public class InfoController {
 		infoService.updateMyInfo(info);
 		return ResponseEntity.ok("내 정보 수정 완료");
 	}
+	
+	
+	/*
+	 * 원래 비밀번호 :
+	 * 바꿀 비밀번호 :
+	 * 바꿀 비밀번호 확인 :
+	 */
+	// 비밀번호 변경
+	@PutMapping("/pass")
+	public ResponseEntity<?> changePassword(@RequestBody @Valid PasswordDTO changePassword){
+		//log.info("비밀번호? {}", changePassword);
+		infoService.changePassword(changePassword);
+		return ResponseEntity.status(HttpStatus.CREATED).build();
+	}
+	
+	
+	// 마이페이지 등급 조회
+	@GetMapping("/grade")
+	public ResponseEntity<InfoDTO> selectMyGrade() {
+		InfoDTO grade = infoService.selectMyGrade();
+		return ResponseEntity.ok(grade);
+	}
+	
 
 }
--- energyplus/src/main/java/com/kh/ecolog/info/model/service/InfoServiceImpl.java
+++ energyplus/src/main/java/com/kh/ecolog/info/model/service/InfoServiceImpl.java
@@ -1,13 +1,19 @@
 package com.kh.ecolog.info.model.service;
 
 import java.lang.ProcessHandle.Info;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.kh.ecolog.auth.model.vo.CustomUserDetails;
 import com.kh.ecolog.auth.service.AuthService;
 import com.kh.ecolog.info.model.dao.InfoMapper;
 import com.kh.ecolog.info.model.dto.InfoDTO;
+import com.kh.ecolog.info.model.dto.PasswordDTO;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -19,6 +25,7 @@ public class InfoServiceImpl implements InfoService {
 	
 	private final InfoMapper infoMapper;
 	private final AuthService authService;
+	private final PasswordEncoder passwordEncoder;
 
 	@Override
 	public InfoDTO selectMyInfo() {
@@ -40,6 +47,39 @@ public class InfoServiceImpl implements InfoService {
 		info.setUserId(userId);
 		
 		infoMapper.updateMyInfo(info);
+	}
+	
+	// 비밀번호 수정
+	@Override
+	public void changePassword(PasswordDTO changePassword) {
+		String encodePassword = passwordEncoder.encode(changePassword.getNewPassword());
+		Long userId = passwordMatches(changePassword.getCurrentPassword());
+		
+		Map<String, Object> changeRequest = new HashMap();
+		changeRequest.put("userId", userId);
+		changeRequest.put("encodePassword", encodePassword);
+		
+		infoMapper.changePassword(changeRequest);
+	}
+	
+	private Long passwordMatches(String password) {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		CustomUserDetails user = (CustomUserDetails) auth.getPrincipal();
+		System.out.println("현재 로그인된 유저: " + user.getUsername());
+		
+		if(!passwordEncoder.matches(password, user.getPassword())) {
+			throw new RuntimeException("비밀번호가 일치하지 않습니다.");
+		}
+		return user.getUserId();
+	}
+	
+	// 내 등급 조회
+	@Override
+	public InfoDTO selectMyGrade() {
+		CustomUserDetails user = authService.getUserDetails();
+		Long userId = user.getUserId();
+		InfoDTO grade = infoMapper.selectMyGrade(userId);
+		return grade;
 	}
 
 }
