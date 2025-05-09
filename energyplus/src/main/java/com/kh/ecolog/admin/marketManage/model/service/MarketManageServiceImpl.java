088a6ad0638b6f2629d620e9bbbb8d86851e883f
parkjin-sol <83712091+parkjin-sol@users.noreply.github.com>
2025-05-09 14:14:38 +0900

    feature/중고거래관리자

--- energyplus/src/main/java/com/kh/ecolog/admin/marketManage/model/service/MarketManageServiceImpl.java
+++ energyplus/src/main/java/com/kh/ecolog/admin/marketManage/model/service/MarketManageServiceImpl.java
@@ -1,12 +1,11 @@
 package com.kh.ecolog.admin.marketManage.model.service;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.stereotype.Service;
 
 import com.kh.ecolog.admin.marketManage.model.dao.MarketManageMapper;
+import com.kh.ecolog.market.board.model.dao.MarketMapper;
 import com.kh.ecolog.market.board.model.dto.MarketDTO;
 
 import lombok.RequiredArgsConstructor;
@@ -16,16 +15,22 @@ import lombok.RequiredArgsConstructor;
 public class MarketManageServiceImpl implements MarketManageService {
 	
 	 private final MarketManageMapper marketManageMapper;
-
-	 public Map<String, Object> findMarketsWithPaging(int page, int size) {
-		    int offset = page * size;
-		    List<MarketDTO> list = marketManageMapper.selectMarketsWithPaging(offset, size);
-		    int totalCount = marketManageMapper.countMarkets();
-
-		    Map<String, Object> result = new HashMap<>();
-		    result.put("list", list);
-		    result.put("totalCount", totalCount);
-		    return result;
-		}
-
+	 private final MarketMapper marketMapper; 
+	 
+	 public List<MarketDTO> findAllMarketsForAdmin() {
+		    return marketManageMapper.findAllMarketsForAdmin();
+	 }
+	 @Override
+	 public MarketDTO findMarketByNo(Long marketNo) {
+	     MarketDTO dto = marketManageMapper.findMarketByNo(marketNo);
+	     dto.setImageList(marketMapper.selectImagesByMarketNo(marketNo));
+	     return dto;
+	 }
+	 @Override
+	 public void toggleMarketHidden(Long marketNo) {
+	     MarketDTO market = marketManageMapper.findMarketByNo(marketNo);
+	     String currentStatus = market.getIsHidden();
+	     String newStatus = currentStatus.equals("Y") ? "N" : "Y";
+	     marketMapper.hideMarket(marketNo, newStatus);
+	 }
 }
